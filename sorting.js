661a8dcaf31af6934e3866a998c9014a91edfa20
Bagus <25834188+contactjavas@users.noreply.github.com>
2020-08-07 14:36:54 +0700

    sorting swaps: well, this is a cheating way, by utilizing array sort

---
+++ sorting.js
@@ -0,0 +1,47 @@
+(function () {
+  window.addEventListener('load', function () {
+    let numbers = document.querySelector('#numbers').value;
+    numbers = numbers.split(',');
+    numbers = numbers.map(x => parseInt(x, 10));
+
+    playSwaps(numbers);
+  });
+
+  document.querySelector('#numbers').addEventListener('keypress', function (e) {
+    let numbers = this.value.split(',');
+    numbers = numbers.map(x => parseInt(x, 10));
+  
+    playSwaps(numbers);
+  });
+
+  function playSwaps(numbers) {
+    console.log(numbers);
+    const sorted = numbers.slice().sort();
+    let count = 0;
+    let i = numbers.length;
+    let p;
+
+    const elms = {
+      process: document.querySelector('#process'),
+      result: document.querySelector('#result'),
+      total: document.querySelector('#total')
+    };
+
+    while (i--) {
+      if (numbers[i] === sorted[i]) {
+        elms.process.innerHTML = elms.process.innerHTML + numbers[i] + ' already in correct position, no need to swap<br>';
+        continue;
+      }
+
+      p = numbers.indexOf(sorted[i]);
+      [numbers[i], numbers[p]] = [numbers[p], numbers[i]];
+      elms.process.innerHTML = elms.process.innerHTML + numbers[i] + ' is in the wrong position, let\'s swap ' + numbers[i] + ' with ' + numbers[p] + '<br>';
+      count++;
+    }
+
+    elms.result.innerHTML = 'sorted result: ' + numbers.toString();
+    elms.total.innerHTML = 'needed swaps: ' + count;
+
+    return count;
+  }
+})();
